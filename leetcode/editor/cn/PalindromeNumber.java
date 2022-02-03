c4b78ac3555958786dc47a6ff23da98f7839b65b
shanjunwei <shanjunwei@xiaohongshu.com>
2022-02-03 16:14:08 +0800

    只是测试

--- leetcode/editor/cn/PalindromeNumber.java
+++ leetcode/editor/cn/PalindromeNumber.java
@@ -30,6 +30,9 @@ package editor.cn;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *  测试修改
+ */
 public class PalindromeNumber {
     public static void main(String[] args) {
         Solution solution = new PalindromeNumber().new Solution();
